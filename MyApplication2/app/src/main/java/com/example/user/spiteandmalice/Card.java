9da0b290e082974944edf07cc04271dba875f88c
RLOG89 <ross.loggie@gmail.com>
2016-11-12 12:35:55 +0000

    constructor and toString method created

---
+++ MyApplication2/app/src/main/java/com/example/user/spiteandmalice/Card.java
@@ -0,0 +1,22 @@
+package com.example.user.spiteandmalice;
+
+/**
+ * Created by user on 12/11/2016.
+ */
+
+public class Card {
+
+    private Rank rank;
+    private Suit suit;
+
+    public Card(Rank rank, Suit suit){
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    @Override
+    public String toString(){
+        return rank + " of " + suit;
+    }
+
+}
