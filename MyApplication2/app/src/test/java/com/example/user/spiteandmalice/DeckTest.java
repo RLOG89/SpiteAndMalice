671dd68c6f47860b50e9a2e7fdbd32c6a7893260
RLOG89 <ross.loggie@gmail.com>
2016-11-12 13:23:22 +0000

    tested that deck is populated with 52 cards

---
+++ MyApplication2/app/src/test/java/com/example/user/spiteandmalice/DeckTest.java
@@ -0,0 +1,27 @@
+package com.example.user.spiteandmalice;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by user on 12/11/2016.
+ */
+
+public class DeckTest {
+
+    Deck deck;
+
+    @Before
+    public void before() {
+        deck = new Deck(new ArrayList<Card>());
+    }
+
+    @Test
+    public void deckIsPopulated(){
+        assertEquals(52, deck.size());
+    }
+}
