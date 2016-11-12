b7a0d4b370cc61059950947fbf46fa27cea6e6ab
RLOG89 <ross.loggie@gmail.com>
2016-11-12 12:50:03 +0000

    tested canGetRank

---
+++ MyApplication2/app/src/test/java/com/example/user/spiteandmalice/CardTest.java
@@ -0,0 +1,25 @@
+package com.example.user.spiteandmalice;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by user on 12/11/2016.
+ */
+
+public class CardTest {
+
+    Card card;
+
+    @Before
+    public void before(){
+        card = new Card(Rank.ACE, Suit.CLUBS);
+    }
+
+    @Test
+    public void canGetRank(){
+        assertEquals(Rank.ACE, card.getRank());
+    }
+}
