9592a7062a6527dc006cfaa10ae3449730f1f063
RLOG89 <ross.loggie@gmail.com>
2016-11-12 13:03:22 +0000

    constructor created

--- MyApplication2/app/src/main/java/com/example/user/spiteandmalice/Deck.java
+++ MyApplication2/app/src/main/java/com/example/user/spiteandmalice/Deck.java
@@ -1,8 +1,16 @@
 package com.example.user.spiteandmalice;
 
+import java.util.ArrayList;
+
 /**
  * Created by user on 12/11/2016.
  */
 
 public class Deck {
+
+    ArrayList<Card> cards;
+
+    public Deck(ArrayList<Card> cards) {
+        this.cards = cards;
+    }
 }
