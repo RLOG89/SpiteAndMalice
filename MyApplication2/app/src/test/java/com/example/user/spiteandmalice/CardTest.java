a99d76654b71625e601e2a438ff94276e82efc21
RLOG89 <ross.loggie@gmail.com>
2016-11-12 12:52:15 +0000

    tested canGetSuit

--- MyApplication2/app/src/test/java/com/example/user/spiteandmalice/CardTest.java
+++ MyApplication2/app/src/test/java/com/example/user/spiteandmalice/CardTest.java
@@ -14,12 +14,17 @@ public class CardTest {
     Card card;
 
     @Before
-    public void before(){
+    public void before() {
         card = new Card(Rank.ACE, Suit.CLUBS);
     }
 
     @Test
-    public void canGetRank(){
+    public void canGetRank() {
         assertEquals(Rank.ACE, card.getRank());
+    }
+
+    @Test
+    public void canGetSuit() {
+        assertEquals(Suit.CLUBS, card.getSuit());
     }
 }
