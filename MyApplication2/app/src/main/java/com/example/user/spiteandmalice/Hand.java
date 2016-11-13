9eabc5953a5abd145ecb391a29fe8227b5bed972
RLOG89 <ross.loggie@gmail.com>
2016-11-13 10:31:47 +0000

    added constructor and addCard method

--- MyApplication2/app/src/main/java/com/example/user/spiteandmalice/Hand.java
+++ MyApplication2/app/src/main/java/com/example/user/spiteandmalice/Hand.java
@@ -1,8 +1,22 @@
 package com.example.user.spiteandmalice;
 
+import java.util.ArrayList;
+
 /**
  * Created by user on 12/11/2016.
  */
 
 public class Hand {
+
+   private ArrayList<Card> cards;
+
+    public Hand() {
+        this.cards = new ArrayList<Card>();
+    }
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    
 }
