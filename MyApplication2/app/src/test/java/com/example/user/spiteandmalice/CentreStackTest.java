eac19b53e4ba6406c49356c5961898119798fbfe
RLOG89 <ross.loggie@gmail.com>
2016-11-14 16:12:25 +0000

    tested addCard method

---
+++ MyApplication2/app/src/test/java/com/example/user/spiteandmalice/CentreStackTest.java
@@ -0,0 +1,29 @@
+package com.example.user.spiteandmalice;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by user on 14/11/2016.
+ */
+
+public class CentreStackTest {
+
+    CentreStack centreStack;
+    Card card;
+
+    @Before
+    public void before() {
+        centreStack = new CentreStack();
+        card = new Card(Rank.KING, Suit.CLUBS);
+    }
+
+    @Test
+    public void canAddCardToCentreStack() {
+        assertEquals(0, centreStack.getStack(1).size());
+        centreStack.addCard(1, card);
+        assertEquals(1, centreStack.getStack(1).size());
+    }
+}
