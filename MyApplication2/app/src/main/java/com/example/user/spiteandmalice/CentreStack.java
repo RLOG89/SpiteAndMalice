e2b26c16d14e7307bbae63b50e338bfcef4f5d66
RLOG89 <ross.loggie@gmail.com>
2016-11-14 17:33:57 +0000

    downsized instances of stack

--- MyApplication2/app/src/main/java/com/example/user/spiteandmalice/CentreStack.java
+++ MyApplication2/app/src/main/java/com/example/user/spiteandmalice/CentreStack.java
@@ -11,36 +11,36 @@ import java.util.ArrayList;
 public class CentreStack {
 
     // put these in an array later //
-    private ArrayList<Card> Stack1 = new ArrayList<>(12);
-    private ArrayList<Card> Stack2 = new ArrayList<>(12);
-    private ArrayList<Card> Stack3 = new ArrayList<>(12);
-    private ArrayList<Card> Stack4 = new ArrayList<>(12);
+    private ArrayList<Card> stack1 = new ArrayList<>(12);
+    private ArrayList<Card> stack2 = new ArrayList<>(12);
+    private ArrayList<Card> stack3 = new ArrayList<>(12);
+    private ArrayList<Card> stack4 = new ArrayList<>(12);
 
     public CentreStack() {
     }
 
     public ArrayList<Card> getStack(int centreStackNumber) {
-        if (centreStackNumber == 1){return Stack1;}
-        if (centreStackNumber == 2){return Stack2;}
-        if (centreStackNumber == 3){return Stack3;}
-        if (centreStackNumber == 4){return Stack4;}
+        if (centreStackNumber == 1){return stack1;}
+        if (centreStackNumber == 2){return stack2;}
+        if (centreStackNumber == 3){return stack3;}
+        if (centreStackNumber == 4){return stack4;}
         // add throw exception here if any other number entered //
         return null;
     }
 
     public void addCard(int centreStackNumber, Card card) {
-        if (centreStackNumber == 1){Stack1.add(card);}
-        if (centreStackNumber == 2){Stack2.add(card);}
-        if (centreStackNumber == 3){Stack3.add(card);}
-        if (centreStackNumber == 4){Stack4.add(card);}
+        if (centreStackNumber == 1){stack1.add(card);}
+        if (centreStackNumber == 2){stack2.add(card);}
+        if (centreStackNumber == 3){stack3.add(card);}
+        if (centreStackNumber == 4){stack4.add(card);}
         // add throw exception here if any other number entered //
         else;
     }
 
     public void resetStack(int centreStackNumber) {
-        if (centreStackNumber == 1){Stack1.clear();}
-        if (centreStackNumber == 2){Stack2.clear();}
-        if (centreStackNumber == 3){Stack3.clear();}
-        if (centreStackNumber == 4){Stack4.clear();}
+        if (centreStackNumber == 1){stack1.clear();}
+        if (centreStackNumber == 2){stack2.clear();}
+        if (centreStackNumber == 3){stack3.clear();}
+        if (centreStackNumber == 4){stack4.clear();}
     }
 }
--- MyApplication2/app/src/main/java/com/example/user/spiteandmalice/SideStack.java
+++ MyApplication2/app/src/main/java/com/example/user/spiteandmalice/SideStack.java
@@ -14,28 +14,28 @@ Cards must be removed from position 0 only, and added to CentreStack
 public class SideStack {
 
     //put these in an array later//
-    private ArrayList<Card> SideStack1 = new ArrayList<>();
-    private ArrayList<Card> SideStack2 = new ArrayList<>();
-    private ArrayList<Card> SideStack3 = new ArrayList<>();
-    private ArrayList<Card> SideStack4 = new ArrayList<>();
+    private ArrayList<Card> sideStack1 = new ArrayList<>();
+    private ArrayList<Card> sideStack2 = new ArrayList<>();
+    private ArrayList<Card> sideStack3 = new ArrayList<>();
+    private ArrayList<Card> sideStack4 = new ArrayList<>();
 
     public SideStack() {
     }
 
     public ArrayList<Card> getSideStack(int sideStackNumber) {
-        if (sideStackNumber == 1){return SideStack1;}
-        if (sideStackNumber == 2){return SideStack2;}
-        if (sideStackNumber == 3){return SideStack3;}
-        if (sideStackNumber == 4){return SideStack4;}
+        if (sideStackNumber == 1){return sideStack1;}
+        if (sideStackNumber == 2){return sideStack2;}
+        if (sideStackNumber == 3){return sideStack3;}
+        if (sideStackNumber == 4){return sideStack4;}
         // add throw exception here if any other number entered //
         return null;
     }
 
     public void addCardToSideStack(int sideStackNumber, Card card) {
-        if (sideStackNumber == 1){SideStack1.add(0, card);}
-        if (sideStackNumber ==2){SideStack2.add(0, card);}
-        if (sideStackNumber ==3){SideStack3.add(0, card);}
-        if (sideStackNumber ==4){SideStack4.add(0, card);}
+        if (sideStackNumber == 1){sideStack1.add(0, card);}
+        if (sideStackNumber == 2){sideStack2.add(0, card);}
+        if (sideStackNumber == 3){sideStack3.add(0, card);}
+        if (sideStackNumber == 4){sideStack4.add(0, card);}
         // add throw exception here if any other number entered //
         else ;
     }
@@ -63,19 +63,19 @@ public class SideStack {
 
     public Card removeCardFromSideStack(int sideStackNumber) {
         if (sideStackNumber == 1) {
-            Card card = SideStack1.remove(0);
+            Card card = sideStack1.remove(0);
             return card;
         }
         else if (sideStackNumber == 2) {
-            Card card = SideStack2.remove(0);
+            Card card = sideStack2.remove(0);
             return card;
         }
         else if (sideStackNumber == 3) {
-            Card card = SideStack3.remove(0);
+            Card card = sideStack3.remove(0);
             return card;
         }
         else if (sideStackNumber == 4) {
-            Card card = SideStack4.remove(0);
+            Card card = sideStack4.remove(0);
             return card;
         }
         // add throw exception here if any other number entered //
