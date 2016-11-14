c05daf8f58e017c5fd2ba1a0f176784f7e103ace
RLOG89 <ross.loggie@gmail.com>
2016-11-14 10:31:40 +0000

    tested new card goes to position 0

--- MyApplication2/app/src/test/java/com/example/user/spiteandmalice/SideStackTest.java
+++ MyApplication2/app/src/test/java/com/example/user/spiteandmalice/SideStackTest.java
@@ -13,6 +13,7 @@ public class SideStackTest {
 
     SideStack sideStack;
     Card card;
+    Card card2;
 
     @Before
     public void before() {
@@ -24,5 +25,14 @@ public class SideStackTest {
     public void canAddCardToSideStack() {
         sideStack.AddCardToSideStack1(card);
         assertEquals(1, sideStack.getSideStack1().size());
+    }
+
+    @Test
+    public void newCardGoesToPosition1() {
+        card2 = new Card(Rank.EIGHT, Suit.DIAMONDS);
+        sideStack.AddCardToSideStack2(card);
+        sideStack.AddCardToSideStack2(card2);
+        assertEquals(2, sideStack.getSideStack2().size());
+        assertEquals("EIGHT of DIAMONDS", sideStack.getSideStack2().get(0).toString());
     }
 }
