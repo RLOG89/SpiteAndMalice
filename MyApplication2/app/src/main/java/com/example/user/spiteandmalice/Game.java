c3dd4391d606a2c9f16ba65bc0ab6c749b026b71
RLOG89 <ross.loggie@gmail.com>
2016-11-15 13:06:30 +0000

    setup constructor

--- MyApplication2/app/src/main/java/com/example/user/spiteandmalice/Game.java
+++ MyApplication2/app/src/main/java/com/example/user/spiteandmalice/Game.java
@@ -1,8 +1,36 @@
 package com.example.user.spiteandmalice;
 
+import java.util.ArrayList;
+
 /**
  * Created by user on 12/11/2016.
  */
 
 public class Game {
+
+    private CentreStack centreStack;
+    private ArrayList<Card> spiteAndMaliceDeck = new ArrayList<>();
+    private ArrayList<Player> players = new ArrayList<>();
+    private Deck deck;
+
+    public Game (CentreStack centreStack, Deck deck, ArrayList<Player> players) {
+        this.centreStack = centreStack;
+        this.deck = deck;
+        this.players = players;
+        createSpiteAndMaliceDeck();
+    }
+
+    public void createSpiteAndMaliceDeck() {
+        for (Card card : deck.getDeck()) {
+            spiteAndMaliceDeck.add(card);
+        }
+    }
+
+//    dealCardsToPlayer() {
+//        for (Player player : players)
+//
+//    }
+
+
+
 }
