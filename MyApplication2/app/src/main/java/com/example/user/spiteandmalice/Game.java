01226495bf7d940e5b88085443243fe3f17cb3e2
RLOG89 <ross.loggie@gmail.com>
2016-11-15 14:18:23 +0000

    added dealCardsToPlayer method

--- MyApplication2/app/src/main/java/com/example/user/spiteandmalice/Game.java
+++ MyApplication2/app/src/main/java/com/example/user/spiteandmalice/Game.java
@@ -12,8 +12,11 @@ public class Game {
     private ArrayList<Card> spiteAndMaliceDeck = new ArrayList<>();
     private ArrayList<Player> players = new ArrayList<>();
     private Deck deck;
+    private Player player;
+    private int maxNumCardsInHand;
 
-    public Game (CentreStack centreStack, Deck deck, ArrayList<Player> players) {
+    public Game (CentreStack centreStack, Deck deck, ArrayList<Player> players, int maxNumCardsInHand) {
+        this.maxNumCardsInHand = maxNumCardsInHand;
         this.centreStack = centreStack;
         this.deck = deck;
         this.players = players;
@@ -39,10 +42,12 @@ public class Game {
     }
 
     public void dealCardsToPlayer() {
-        for (Player player : players)
-
+        int currentCards = player.getHandSize();
+        int last = (spiteAndMaliceDeck.size() - 1);
+        for (Player player : players);
+            while (currentCards < this.maxNumCardsInHand)
+                player.getHand().addCard(spiteAndMaliceDeck.remove(last));
     }
-
 
 
 }
