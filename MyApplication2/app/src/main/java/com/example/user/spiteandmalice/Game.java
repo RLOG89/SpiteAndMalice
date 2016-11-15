18717a1e7e1fbdebad682ac34b53af6630e34c6e
RLOG89 <ross.loggie@gmail.com>
2016-11-15 14:38:32 +0000

    added dealCardsToPlayerHand, dealCardsToPlayerPile and setup methods

--- MyApplication2/app/src/main/java/com/example/user/spiteandmalice/Game.java
+++ MyApplication2/app/src/main/java/com/example/user/spiteandmalice/Game.java
@@ -14,13 +14,17 @@ public class Game {
     private Deck deck;
     private Player player;
     private int maxNumCardsInHand;
+    private int maxNumCardsInPile;
+    private int last = (spiteAndMaliceDeck.size() - 1);
 
-    public Game (CentreStack centreStack, Deck deck, ArrayList<Player> players, int maxNumCardsInHand) {
+    public Game (CentreStack centreStack, Deck deck, ArrayList<Player> players, int maxNumCardsInHand, int maxNumCardsInPile) {
         this.maxNumCardsInHand = maxNumCardsInHand;
+        this.maxNumCardsInPile = maxNumCardsInPile;
         this.centreStack = centreStack;
         this.deck = deck;
         this.players = players;
         createSpiteAndMaliceDeck();
+        setup();
     }
 
     public void createSpiteAndMaliceDeck() {
@@ -41,12 +45,24 @@ public class Game {
         return players;
     }
 
-    public void dealCardsToPlayer() {
+    public void dealCardsToPlayerHand() {
         int currentCards = player.getHandSize();
-        int last = (spiteAndMaliceDeck.size() - 1);
         for (Player player : players);
             while (currentCards < this.maxNumCardsInHand)
                 player.getHand().addCard(spiteAndMaliceDeck.remove(last));
+    }
+
+    public void dealCardsToPlayerPile() {
+        int currentPile = player.getPileSize();
+        for (Player player : players);
+            while (currentPile < this.maxNumCardsInPile)
+                player.getPayOffPile().addCard(spiteAndMaliceDeck.remove(last));
+
+    }
+
+    public void setup() {
+        dealCardsToPlayerHand();
+        dealCardsToPlayerPile();
     }
 
 
